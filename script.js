51bea760ae756c96681173caea931751c97fe8c4
MiguelMauricioBuss <125047226+MiguelMauricioBuss@users.noreply.github.com>
2024-04-19 23:43:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function linkd(){
+    window.open("https://www.linkedin.com/in/miguel-mauricio-buss-841469257/")
+}
+function insta(){
+    window.open("https://www.instagram.com/buss.mg/")
+}
+function github(){
+    window.open("https://github.com/MiguelMauricioBuss")
+}
